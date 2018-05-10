56b86304b270437528c6593fd5e6136906007b7b
Andchen <1476204794@qq.com>
2018-05-10 10:49:41 +0800

    class Array<T>(val size: Int)

---
+++ src/Generic/Box.kt
@@ -0,0 +1,14 @@
+package Generic
+
+class Box<T>(t: T)
+{
+	var value = t
+}
+
+fun main(args: Array<String>)
+{
+	val box1: Box<String> = Box("Tom")
+	val box2: Box<Int> = Box(20)
+	println(box1.value)
+	println(box2.value)
+}
